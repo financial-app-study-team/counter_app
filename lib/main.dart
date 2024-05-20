ab126cbd1cc3a1e5e87d9ed1fb5c13c206daded8
진수민 <jimmy@jinsumin-ui-MacBookPro.local>
2024-05-20 09:32:16 +0900

    stateful widget, setState method

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,41 @@ class MyApp extends StatefulWidget {
 }
 
 class _AppState extends State<StatefulWidget> {
+  int counter = 0;
+
+  void onClicked() {
+    setState(() {
+      counter = counter + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: const Color(0xFFF4EDDB),
-        body: Container(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Counter++',
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                '$counter',
+                style: const TextStyle(fontSize: 30),
+              ),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClicked,
+                icon: const Icon(
+                  Icons.add_box_rounded,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
